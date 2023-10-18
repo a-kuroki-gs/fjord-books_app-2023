d1c02aac3afa893d0505271933337ab81310183a
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-10-18 11:28:58 +0900

    リダイレクトできるようにする
    ルーティングを記載し、コントローラを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get       'users/sign_out', to:     'devise/sessions#destroy'
+    post      'users',          to:     'users/registrations#create'
+    get       'users/:id/edit', to:     'users/registrations#edit',      as: :edit_user
+    patch     'users/:id',      to:     'users/registrations#update'
+    delete    'users/:id',      to:     'users#destroy'
+  end
+
+  root 'books#index'
 end
