1874fc9fa579247b9ecafd62a24f398e43d8cec6
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-10-18 11:40:28 +0900

    ユーザー一覧、詳細を表示する
    ビューを作成しルーティングを追加する
    詳細は自分の情報のみ編集・削除できるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get       'users/sign_out', to:     'devise/sessions#destroy'
+    get       'users',          to:     'users#index',                   as: :users
     post      'users',          to:     'users/registrations#create'
     get       'users/:id/edit', to:     'users/registrations#edit',      as: :edit_user
+    get       'users/:id',      to:     'users#show',                    as: :user
     patch     'users/:id',      to:     'users/registrations#update'
     delete    'users/:id',      to:     'users#destroy'
   end
