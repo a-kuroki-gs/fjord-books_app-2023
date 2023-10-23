3c65e48b0112933f02ea262a505d2c04ad74f949
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-10-23 13:48:06 +0900

    日報関連のコントローラの作成

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+class ReportsController < ApplicationController
+  def index
+    @reports = Report.all
+  end
+
+  def show
+    @report = Report.find(params[:id])
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def edit
+    @report = Report.find(params[:id])
+  end
+
+  def create
+    @report = Report.new(report_params)
+
+    respond_to do |format|
+      if @report.save
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
+        format.json { render :show, status: :created, location: @report }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @report.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @report = Report.find(params[:id])
+
+    respond_to do |format|
+      if @report.update(report_params)
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
+        format.json { render :show, status: :ok, location: @report }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @report.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @report = Report.find(params[:id])
+
+    @report.destroy
+
+    respond_to do |format|
+      format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :content, :created_at)
+  end
+end
