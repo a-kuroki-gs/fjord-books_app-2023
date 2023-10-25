e08acc74b0850b24cd886065ddfeb5bb68c6b7fe
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-10-25 10:21:14 +0900

    日報にコメントをつける機能を実装しました

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+#   def edit
+
+#   end
+
+  def create
+    @report = Report.find(params[:report_id])
+    @comment = @report.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    @comment.imageable_id = @report.id
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { redirect_to report_url(@report), status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+#   def update
+
+#   end
+
+#   def destroy
+
+#   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :imageable_id)
+  end
+end
