6c86a7ba38ea0b2a24da4729172bfcaed8ebe58a
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-11-15 13:46:03 +0900

    fixturesを使うように修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,32 +3,37 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
+  fixtures :users, :reports
+
   def setup
-    @user = User.create!(email: 'user@example.com', password: 'password')
-    @report = @user.reports.build(id: 1, title: 'title', content: 'content')
-    @report.save!
+    @user = users(:alice)
   end
 
   test 'editable?' do
+    @report = reports(:one)
+
     assert @report.editable?(@user)
   end
 
   test 'created_on' do
+    @report = reports(:one)
+
     assert_equal @report.created_at.to_date, @report.created_on
   end
 
   test 'save_mentions' do
-    report = @user.reports.build(id: 2, title: 'title', content: 'content')
-    report.save!
+    @user = users(:bob)
+    @first_report = reports(:one)
+    @second_report = reports(:two)
 
-    mention_report = @user.reports.build(title: 'title', content: 'http://localhost:3000/reports/1')
+    @report = @user.reports.build(title: 'MyString', content: "http://localhost:3000/reports/#{@first_report.id}")
 
-    assert_empty mention_report.mentioning_reports
-    mention_report.save!
-    assert_equal mention_report.mentioning_reports.first, Report.find(1)
+    assert_empty @report.mentioning_reports
+    @report.save!
+    assert_equal @report.mentioning_reports.first, @first_report
 
-    mention_report.update!(content: 'http://localhost:3000/reports/2')
-    mention_report.mentioning_reports.reload
-    assert_equal mention_report.mentioning_reports.first, Report.find(2)
+    @report.update!(content: "http://localhost:3000/reports/#{@second_report.id}")
+    @report.mentioning_reports.reload
+    assert_equal @report.mentioning_reports.first, @second_report
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'name_or_email' do
-    user = User.new(email: 'foo@example.com', name: '')
-    assert_equal 'foo@example.com', user.name_or_email
+  fixtures :users
 
-    user.name = 'Foo Bar'
-    assert_equal 'Foo Bar', user.name_or_email
+  test 'name_or_email' do
+    user = users(:alice)
+    assert_equal 'alice', user.name_or_email
+
+    user = users(:bob)
+    assert_equal 'bob@example.com', user.name_or_email
   end
 end
